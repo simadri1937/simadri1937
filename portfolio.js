9d2d9e77b5f3072c5046fe7f724a46bab30b33a4
simadri1937 <104165579+simadri1937@users.noreply.github.com>
2022-04-22 00:25:19 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+
+    $('#menu').click(function(){
+        $(this).toggleclass('fa-times');
+        $('header').toggleclass('toggle');
+    });
+
+    $(window).on('scroll load',function(){
+        $('#menu').removeclass('fa-times');
+        $('header').removeclass('toggle');
+    });
+
+     $('a[href*="#"]').on('click',function(e){
+
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop : $($(this).attr('href')).offset().top,
+
+        },
+        500,
+        'linear'
+        );
+
+     });
+
+
+
+});
